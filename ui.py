315004f443755d01a90eead43cccd0b0436148a3
vishal-manoj <vishalmanoj612@gmail.com>
2022-12-19 15:32:44 +0530

    changed main label width

--- ui.py
+++ ui.py
@@ -51,7 +51,7 @@ class Ui:
         self.root = root
         self.root.title('Insolvency Predictor')
         self.root.state('zoomed')
-        self.root.name_label = Label(self.root, text='Insolvency Predictor', bg='#31f1f7', width=140, height=2, font=10)
+        self.root.name_label = Label(self.root, text='Insolvency Predictor', bg='#31f1f7', width=180, height=2, font=10)
         self.root.name_label.place(x=0, y=0)
         self.button_frame = Frame(self.root, height=740, width=300, highlightbackground='#31f1f7', highlightthickness=4)
         self.button_frame.place(x=0, y=50)
